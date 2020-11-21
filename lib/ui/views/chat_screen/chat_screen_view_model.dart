c0fba36d64639b6c7a3134b699d8fb18afcc30f1
xuhui <944808666@qq.com>
2020-11-21 21:42:54 +0800

    1

--- lib/ui/views/chat_screen/chat_screen_view_model.dart
+++ lib/ui/views/chat_screen/chat_screen_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:event_bus/event_bus.dart';
 import 'package:logging/logging.dart';
 import 'package:provider_start/core/proto/protobuf_gen/message.pb.dart';
@@ -10,6 +12,7 @@ class ChatScreenViewModel extends BaseViewModel {
   final _chatStateService = locator<ChatStateService>();
   final EventBus _eventBus = locator<EventBus>();
   final _log = Logger('ChatScreenViewModel');
+  StreamSubscription<Message> _stream;
   User get currentUser => _chatStateService.currentUser;
   User get chatUser => _chatStateService.chatUser;
   List<Message> get msg => _chatStateService.msg;
@@ -18,7 +21,7 @@ class ChatScreenViewModel extends BaseViewModel {
   }
   bool setIsChatScreenOpen;
   Future<void> init() async {
-    _eventBus.on<Message>().listen((model) {
+    _stream = _eventBus.on<Message>().listen((model) {
       print('接受到消息:$model');
       onAddMessage(model,false);
     });
@@ -27,8 +30,15 @@ class ChatScreenViewModel extends BaseViewModel {
     setBusy(false);
   }
 
+
   void onAddMessage(Message message,bool send) {
     _chatStateService.addMessage(message,send);
     notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _stream.cancel();
   }
 }
