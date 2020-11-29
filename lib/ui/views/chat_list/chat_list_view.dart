ef2997b6c38c39efdbf0aba6a5ed22ccd4ef3949
xuhui <944808666@qq.com>
2020-11-29 20:06:08 +0800

    1

--- lib/ui/views/chat_list/chat_list_view.dart
+++ lib/ui/views/chat_list/chat_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider_start/core/constant/app_icon.dart';
 import 'package:provider_start/core/constant/theme.dart';
 import 'package:provider_start/core/utils/utility.dart';
@@ -40,44 +41,126 @@ class _ChatListViewState extends State<ChatListView> {
 }
 
 class _ChatListBody extends ViewModelWidget<ChatListViewModel> {
-  Widget _userCard(ChatListViewModel model, int index) {
-    return Container(
-      color: Colors.white,
-      child: ListTile(
-          contentPadding: EdgeInsets.symmetric(horizontal: 10),
-          onTap: (){
-            model.pushChatScreen(model.contacts.users[index]);
-          },
-          leading: RippleButton(
-            onPressed: () => {},
-            borderRadius: BorderRadius.circular(28),
-            child: Container(
-              height: 56,
-              width: 56,
+  Widget _item(BuildContext context, ChatListViewModel model, int index) {
+    return GestureDetector(
+      onTap: () {
+        model.pushChatScreen(model.contacts.users[index]);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: ScreenUtil().setHeight(20),
+          vertical: ScreenUtil().setWidth(15),
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(2),
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.white, width: 2),
-                borderRadius: BorderRadius.circular(28),
-                image: DecorationImage(
-                    image: customAdvanceNetworkImage(
-                      model.contacts.users[index].avatar ?? dummyProfilePic,
-                    ),
-                    fit: BoxFit.cover),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: CircleAvatar(
+                radius: ScreenUtil().setHeight(56),
+                backgroundImage:
+                    NetworkImage(model.contacts.users[index].avatar),
               ),
             ),
-          ),
-          title: TitleText(
-            model.contacts.users[index].nickname ?? 'NA',
-            fontSize: 16,
-            fontWeight: FontWeight.w800,
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: CustomText(
-            title: model.contacts.users[index].nickname,
-            style: onPrimarySubTitleText.copyWith(color: Colors.black54),
-          ),
-          trailing: Text(
-            getChatTime('2020-10-27 20:05:09').toString(),
-          )),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.65, //
+              padding: EdgeInsets.only(
+                left: ScreenUtil().setWidth(30),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            model.contacts.users[index].nickname,
+                            style: TextStyle(
+                              fontSize: ScreenUtil().setHeight(26),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          true //chat.sender.isOnline
+                              ? Container(
+                                  margin: const EdgeInsets.only(left: 5),
+                                  width: ScreenUtil().setWidth(12),
+                                  height: ScreenUtil().setHeight(12),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                )
+                              : Container(
+                                  child: null,
+                                ),
+                        ],
+                      ),
+                      Text(
+                        'now', //chat.time,
+                        style: TextStyle(
+                          fontSize: ScreenUtil().setHeight(25),
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil().setHeight(10),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          '最后的信息', //chat.text,
+                          style: TextStyle(
+                            fontSize: ScreenUtil().setHeight(23),
+                            color: Colors.black54,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                      Container(
+                        height: ScreenUtil().setHeight(30),
+                        padding: EdgeInsets.all(ScreenUtil().setHeight(4)),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF9E9E9E), // 底色
+                          borderRadius: BorderRadius.circular(ScreenUtil().setHeight(15)), // 圆角度
+                          //borderRadius: BorderRadius.vertical(top: Radius.elliptical(20, 50)), // 也可控件一边圆角大小
+                        ),
+                        child: Container(
+                          width: ScreenUtil().setWidth(50),
+                          child: Text(
+                            '100+',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white, // 底色
+                              fontSize: ScreenUtil().setWidth(20)
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -86,7 +169,7 @@ class _ChatListBody extends ViewModelWidget<ChatListViewModel> {
     return ListView.separated(
       physics: BouncingScrollPhysics(),
       itemCount: model.contacts.users.length,
-      itemBuilder: (context, index) => _userCard(model, index),
+      itemBuilder: (context, index) => _item(context, model, index),
       separatorBuilder: (context, index) {
         return Divider(
           height: 0,
